d2dbd0f22ab27e0c30026296adb881199f7a49ef
Taj Bortz <taj.bortz@gmail.com>
2016-03-28 23:37:51 -0700

    I don't know why this doesn't work.

--- csv_module.rb
+++ csv_module.rb
@@ -23,10 +23,16 @@ module ActsAsCsv
 
     def each(&block)
       @csv_contents.each do |row|
-        #r = CsvRow.new(@headers, row)
-        #block.call r
-        block.call CsvRow.new(@headers, row)
+        r = CsvRow.new(@headers, row)
+        block.call r
       end
+    end
+    
+    def each1(&block)
+      r = CsvRow.new(@headers, @csv_contents[0])
+      block.call r
+      r = CsvRow.new(@headers, @csv_contents[1])
+      block.call r
     end
 
     attr_accessor :headers, :csv_contents
@@ -60,10 +66,18 @@ puts m.csv_contents.inspect
 puts
 
 puts "Testing CsvRow"
-r = CsvRow.new(m.headers,m.csv_contents[1])
+r = CsvRow.new(m.headers,m.csv_contents[0])
 puts r.one
 puts
 
 puts "Day 3 Self-Study"
+puts "Why doesn't this work ..."
 puts m.each {|row| puts "one [" + row.one + "]"}
 puts
+
+puts "But these do ..."
+m.csv_contents.each do |row|
+  r = CsvRow.new(m.headers, row)
+  puts r.one
+end
+puts m.each1 {|row| puts row.one} 
