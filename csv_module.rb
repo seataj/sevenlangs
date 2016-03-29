c6fd8baf552feb78f7f04e1ead3df14d5cda67f2
Taj Bortz <taj.bortz@gmail.com>
2016-03-28 23:17:25 -0700

    changes

--- csv_module.rb
+++ csv_module.rb
@@ -21,6 +21,14 @@ module ActsAsCsv
       end
     end
 
+    def each(&block)
+      @csv_contents.each do |row|
+        #r = CsvRow.new(@headers, row)
+        #block.call r
+        block.call CsvRow.new(@headers, row)
+      end
+    end
+
     attr_accessor :headers, :csv_contents
     def initialize
       read
@@ -29,12 +37,15 @@ module ActsAsCsv
 end
 
 class CsvRow
-  def self.method_missing name, *args
-    values[name.to_s]
+  def method_missing(methId)
+    header = methId.id2name
+    @values[header]
   end
 
-  def initialize
-    @values = []
+  attr_accessor :values
+  def initialize(headers=[], contents=[])
+    @values={}
+    headers.each_with_index {|header,index| @values[header] = contents[index]}
   end
 end
 
@@ -46,3 +57,13 @@ end
 m = RubyCsv.new
 puts m.headers.inspect
 puts m.csv_contents.inspect
+puts
+
+puts "Testing CsvRow"
+r = CsvRow.new(m.headers,m.csv_contents[1])
+puts r.one
+puts
+
+puts "Day 3 Self-Study"
+puts m.each {|row| puts "one [" + row.one + "]"}
+puts
