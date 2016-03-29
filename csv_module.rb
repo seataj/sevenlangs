8abe4b326b8b7c87452abbeaaf1f17995102ff30
Taj Bortz <taj.bortz@gmail.com>
2016-03-29 00:11:21 -0700

    I did it!

--- csv_module.rb
+++ csv_module.rb
@@ -17,42 +17,19 @@ module ActsAsCsv
       @headers = file.gets.chomp.split(', ')
 
       file.each do |row|
-        @csv_contents << row.chomp.split(', ')
+        row_a = row.chomp.split(', ')
+        @csv_contents << row_a
+        @rows << CsvRow.new(@headers, row_a)
       end
     end
 
     def each(&block)
-      @csv_contents.each do |row|
-        r = CsvRow.new(@headers, row)
-        block.call r
-      end
+      @rows.each {|row| block.call row}
     end
     
-    def eachx(&block)
-      @csv_contents.each do |row|
-        r = CsvRow.new(@headers, row)
-        block.call r
-      end
-      puts "huh"
-    end
-    
-    def eachy(&block)
-      tests = ["test a", "test b", "test c"]
-      tests.each do |test|
-        block.call test
-      end
-      nil
-    end
-    
-    def each1(&block)
-      r = CsvRow.new(@headers, @csv_contents[0])
-      block.call r
-      r = CsvRow.new(@headers, @csv_contents[1])
-      block.call r
-    end
-
     attr_accessor :headers, :csv_contents
     def initialize
+      @rows = []
       read
     end
   end
@@ -81,22 +58,7 @@ puts m.headers.inspect
 puts m.csv_contents.inspect
 puts
 
-puts "Testing CsvRow"
-r = CsvRow.new(m.headers,m.csv_contents[0])
-puts r.one
-puts
-
 puts "Day 3 Self-Study"
-puts "Why doesn't this work ..."
-puts m.each {|row| puts "one [" + row.one + "]"}
+m.each {|row| puts row.one}
 puts
-puts "And this is weird ..."
-puts m.eachx {|row| puts "one [" + row.one + "]"}
-puts m.eachy {|header| puts header}
-puts
-puts "But these do ..."
-m.csv_contents.each do |row|
-  r = CsvRow.new(m.headers, row)
-  puts r.one
-end
-puts m.each1 {|row| puts row.one} 
+m.each {|row| puts row.two}
